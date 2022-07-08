062eb0809a6f85e5e2893aac948665c2fd28be90
feddericahu99 <federica.hu339@gmail.com>
2022-07-08 15:44:17 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 /*
- Descrizione:
-    Visualizzare in pagina 5 numeri casuali  diversi tra loro. 
-    Da lì parte un timer di 30 secondi.
-    Dopo 30 secondi l'utente deve inserire, uno alla volta, i 
-    numeri che ha visto precedentemente, tramite i prompt().
-    Dopo che sono stati inseriti i 5 numeri, il software dice 
-    quanti e quali dei numeri da indovinare sono stati individuati.
- Esecuzione:
-    1- Creo un randomizer attraverso un function
-    2- Faccio partire i trenta secondi
-    3- Inserisco dei prompt per chiedere dei numeri all'user
+Descrizione:
+Visualizzare in pagina 5 numeri casuali  diversi tra loro. 
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i 
+numeri che ha visto precedentemente, tramite i prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice 
+quanti e quali dei numeri da indovinare sono stati individuati.
+Esecuzione:
+1- Creo un randomizer attraverso un function
+2- Faccio partire i trenta secondi
+3- Inserisco dei prompt per chiedere dei numeri all'user
 */
 
 // funzioni
@@ -17,12 +17,15 @@ function getRandomnumber() {
     while ( numberToMemorize.length < 5 ){
         let randomic = 0;
         randomic= Math.floor(Math.random()* max +1 - min) + min; 
-
+        
         if (!numberToMemorize.includes(randomic)) {  //controllo se la bomba non è presente nell'array
             numberToMemorize.push(randomic);         // se NON è nella lista allora pusha.
         } 
     }
 }
+// array di appoggio
+const numberToMemorize= [];
+console.log(numberToMemorize);
 
 // mi porto gli elementi dal DOM
 let remainingSeconds = document.getElementById('remaining-sec');
@@ -32,7 +35,7 @@ const start = document.getElementById('start');
 
 // far partire il timer 
 start.addEventListener ('click', function() {
-
+   
 // numeri random
     getRandomnumber(min=0, max=100);
 //porto in DOM
@@ -57,18 +60,28 @@ start.addEventListener ('click', function() {
     if (remainingSeconds=0) {clearInterval(setTimeout)};
     }, 30000);
 
+
+    const userAnswer= [];
+
+    setTimeout(() => {
+        for (i= 1; i = numberToMemorize.length; i++ ) {
+            const answers= parseInt(prompt('scrivi i numeri uno alla volta:'))
+            userAnswer.push(answers);
+        }
+       }, 31000)
+
+       
+       //verifica di vittoria
+       
+       if(userAnswer == numberToMemorize) {
+           console.log('Hai perso');
+       } else {
+           console.log('Hai vinto')
+       }
 })
 
-// array di appoggio
-const numberToMemorize= [];
-console.log(numberToMemorize);
 
-setTimeout(() => {
-    for ( i = 0 ; i < numberToMemorize.lenght; i++); {
-    const answers= prompt( 'inserisci le risposte (uno alla volta):');
-    console.log(answers)
-    }
-   }, 32000)
+
 
 
 
